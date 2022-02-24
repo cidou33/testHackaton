05d16b99bf118b1e543f15446b66f7cf7c19be46
cedric ducros <cedricducros1@gmail.com>
2022-02-24 15:26:04 +0100

    commit pour changer de branche dernier jour

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,9 @@
-// We first grab our scroll area and carousel buttons
 const carouselScrollArea = document.querySelector(".CDcarousel__scroll-area");
-
 const leftCarouselButton = document.querySelector(".CDcarousel__button--left");
-
 const rightCarouselButton = document.querySelector(".CDcarousel__button--right");
+const carouselWrapper = document.querySelector(".CDcarousel__wrapper");
 
-// We then listen in for clicks and scroll the carousel accordingly
 leftCarouselButton.addEventListener("click", () => scrollCarousel("left"));
-
 rightCarouselButton.addEventListener("click", () => scrollCarousel("right"));
 
 function scrollCarousel(direction) {
@@ -20,12 +16,6 @@ function scrollCarousel(direction) {
   }
 }
 
-// We grab the whole carousel wrapper
-const carouselWrapper = document.querySelector(".CDcarousel__wrapper");
-
-// We listen for keypresses and react based on which key was pressed
-carouselWrapper.addEventListener("keydown", handleKeyDown);
-
 function handleKeyDown(e) {
   if (event.key === "ArrowLeft") {
     scrollCarousel("left");
@@ -33,3 +23,6 @@ function handleKeyDown(e) {
     scrollCarousel("right");
   }
 }
+
+carouselWrapper.addEventListener("keydown", handleKeyDown);
+
